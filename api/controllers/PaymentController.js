27bacf5c10295fadfc286af0a12dcbe826eea274
emmanuelyupit <emmanuelyupit08@gmail.com>
2017-02-23 13:05:30 -0500

    conekta test succeeded

--- api/controllers/PaymentController.js
+++ api/controllers/PaymentController.js
@@ -6,6 +6,33 @@ var CLIENT_BALANCE_TYPE = 'client-balance';
 var EWALLET_NEGATIVE = 'negative';
 var CANCELLED_STATUS = 'cancelled';
 var PAYMENT_CANCEL_TYPE = 'cancellation';
+var conekta = require('conekta');
+
+conekta.api_key = '9YxqfRnx4sMQDnRsqdYn';
+conekta.locale = 'es';
+
+conekta.Order.create({
+    "currency": "MXN",
+    "customer_info": {
+        "name": "Jul Ceballos",
+        "phone": "+5215555555555",
+        "email": "jul@conekta.io"
+    },
+    "line_items": [{
+        "name": "Box of Cohiba S1s",
+        "description": "Imported From Mex.",
+        "unit_price": 35000,
+        "quantity": 1,
+        "tags": ["food", "mexican food"],
+        "type": "physical"
+    }]
+  }, function(err, res) {
+    if (err) {
+        console.log(err.type);
+        return;
+    }
+    console.log(res.toObject());
+});
 
 module.exports = {
 
@@ -122,12 +149,12 @@ module.exports = {
   },
 
 
+
   getPaymentGroups: function(req, res){
     var paymentGroups = PaymentService.getPaymentGroups();
     res.json(paymentGroups);
   }	
 };
-
 
 function formatProductsIds(details){
   var result = [];
