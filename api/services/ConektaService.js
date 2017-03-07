c61a6ef1d806a17da4335a1a585ae6a91fb557e1
emmanuelyupit <emmanuelyupit08@gmail.com>
2017-03-07 12:24:47 -0500

    conekta charges

--- api/services/ConektaService.js
+++ api/services/ConektaService.js
@@ -1,32 +1,43 @@
 var conekta = require('conekta');
 conekta.api_key = '9YxqfRnx4sMQDnRsqdYn';
 conekta.locale = 'es';
+conekta.api_version = '2.0.0';
+
 
 module.exports = {
-	createOrder: createOrder
+	//createOrder: createOrder
 };
 
-function createOrder(){
-	conekta.Order.create({
-	    "currency": "MXN",
-	    "customer_info": {
-	        "name": "Jul Ceballos",
-	        "phone": "+5215555555555",
-	        "email": "jul@conekta.io"
-	    },
-	    "line_items": [{
-	        "name": "Box of Cohiba S1s",
-	        "description": "Imported From Mex.",
-	        "unit_price": 35000,
-	        "quantity": 1,
-	        "tags": ["food", "mexican food"],
-	        "type": "physical"
-	    }]
-	  }, function(err, res) {
-	    if (err) {
-	        console.log(err.type);
-	        return;
-	    }
-	    console.log(res.toObject());
-	});	
+conekta.Order.create({
+  "currency": "mxn",
+  "customer_info": {
+		"name": "Jul Ceballos",
+        "phone": "+5215555555555",
+        "email": "jul@conekta.io"  
+    },
+  "line_items": [{
+    "name": "Box of Cohiba S1s",
+    "unit_price": 35000,
+    "quantity": 1
+  }],
+
+}, function(err, res) {
+      var order =  res.toObject();
+      console.log('ID', order.id);
+      return order;
+});
+
+var params = {
+	"payment_method": {
+		"type": "card",
+		"expires_at": 1479167175
+	},
+	"amount": 350000
 }
+
+conekta.Order.find("ord_2g8ipqs4pzpfQMvqC", function(err, order) {
+    order.createCharge(params, function(err, charge) {
+        console.log(charge);
+    });
+});
+//console.log("Conekta Order: ", order);
