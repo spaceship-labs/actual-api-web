dad556c718122c5d804f52b237af2d6501b1f0ee
emmanuelyupit <emmanuelyupit08@gmail.com>
2017-03-07 12:42:56 -0500

    conekta charge harcoded

--- api/services/ConektaService.js
+++ api/services/ConektaService.js
@@ -5,39 +5,40 @@ conekta.api_version = '2.0.0';
 
 
 module.exports = {
-	//createOrder: createOrder
+	createOrder: createOrder
 };
+function createOrder (argument) {
+	conekta.Order.create({
+	  "currency": "mxn",
+	  "customer_info": {
+			"name": "Jul Ceballos",
+	        "phone": "+5215555555555",
+	        "email": "jul@conekta.io"  
+	    },
+	  "line_items": [{
+	    "name": "Box of Cohiba S1s",
+	    "unit_price": 35000,
+	    "quantity": 1
+	  }],
 
-conekta.Order.create({
-  "currency": "mxn",
-  "customer_info": {
-		"name": "Jul Ceballos",
-        "phone": "+5215555555555",
-        "email": "jul@conekta.io"  
-    },
-  "line_items": [{
-    "name": "Box of Cohiba S1s",
-    "unit_price": 35000,
-    "quantity": 1
-  }],
+	}, function(err, res) {
+	      var order =  res.toObject();
+	      console.log('ID', order.id);
+	      return order;
+	});
+},
 
-}, function(err, res) {
-      var order =  res.toObject();
-      console.log('ID', order.id);
-      return order;
-});
-
-var params = {
-	"payment_method": {
-		"type": "card",
-		"expires_at": 1479167175
-	},
-	"amount": 350000
+function chargeOrder() {
+	var params = {
+		"payment_method": {
+			"type": "card",
+			"expires_at": 1479167175
+		},
+		"amount": 350000
+	}
+	conekta.Order.find("ord_2g8ipqs4pzpfQMvqC", function(err, order) {
+	    order.createCharge(params, function(err, charge) {
+	        console.log(charge);
+	    });
+	}); 
 }
-
-conekta.Order.find("ord_2g8ipqs4pzpfQMvqC", function(err, order) {
-    order.createCharge(params, function(err, charge) {
-        console.log(charge);
-    });
-});
-//console.log("Conekta Order: ", order);
