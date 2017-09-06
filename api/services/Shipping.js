04dde3c7a80daee0ef385dd2851e447e22234b2e
Luis <luis19prz@gmail.com>
2017-09-06 18:30:04 -0500

    low season days

--- api/services/Shipping.js
+++ api/services/Shipping.js
@@ -162,7 +162,8 @@ function buildShippingItem(stockItem, storeWarehouseId, zipcodeDelivery, product
 
   return Season.findOne(seasonQuery)
     .then(function(season){
-      var seasonDays   = (season && season.Days) || 7;
+      var LOW_SEASON_DAYS = 10; //Original: 7
+      var seasonDays   = (season && season.Days) || LOW_SEASON_DAYS;
       var days = productDays + zipcodeDays;
       //var days = productDays + seasonDays + zipcodeDays;
       /*
