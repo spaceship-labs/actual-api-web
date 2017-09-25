bc8c377bc7e690c9d0e066a6eac3d5f7ebc171c0
Luis <luis19prz@gmail.com>
2017-09-25 18:51:49 -0500

    banners find populate in site findByHandle

--- api/controllers/SiteController.js
+++ api/controllers/SiteController.js
@@ -2,8 +2,13 @@ module.exports = {
   findByHandle: function(req, res){
     var form = req.params.all();
     var handle = form.handle;
-    Site.findOne({handle:handle})
-      .then(function(site){
+    var findPromise = Site.findOne({handle:handle})
+    
+    if(form.getBanners){
+      findPromise.populate('Banners');
+    }
+
+    findPromise.then(function(site){
         res.json(site);
       }).catch(function(err){
         console.log(err);
