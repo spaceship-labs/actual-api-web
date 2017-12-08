43d2d1e8cdbe93b597dd130d5bc3c02b8a79dd9e
Luis <luis19prz@gmail.com>
2017-12-08 17:31:07 -0500

    currentstore fix param id

--- api/services/PaymentService.js
+++ api/services/PaymentService.js
@@ -176,7 +176,7 @@ function getMethodGroupsWithTotals(quotationId, activeStore, options){
       update: false,
       paymentGroup: mG.group,
     };
-    params.currentStore = activeStore.id;
+    params.currentStoreId = activeStore.id;
     var calculator = QuotationService.Calculator();
     return calculator.getQuotationTotals(id, params);
   });
