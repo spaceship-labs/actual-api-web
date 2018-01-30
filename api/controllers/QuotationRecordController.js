dd4b921e4528e7cc6e4fabde5a706b237894ceb2
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-01-29 23:36:25 -0500

    QuotationRecord api added

---
+++ api/controllers/QuotationRecordController.js
@@ -0,0 +1,52 @@
+/**
+ * QuotationRecordController
+ *
+ * @description :: Server-side logic for managing Quotationrecords
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const id = req.param('id');
+      const records = await QuotationRecord.find({ Quotation: id })
+        .populate('User')
+        .populate('files');
+      res.ok(records);
+    } catch (err) {
+      res.negotiate(err);
+    }
+  },
+
+  async create(req, res) {
+    try {
+      const id = req.param('id');
+      const { eventType, dateTime, notes } = req.allParams();
+      if (req.user.role !== 'admin')
+        throw new Error({
+          status: 400,
+          message: 'Este usuario no es administrador'
+        });
+      const quotationRecord = await QuotationRecord.create({
+        Quotation: id,
+        User: req.user.id,
+        eventType,
+        dateTime,
+        notes
+      });
+      if (req._fileparser.upstreams.length)
+        await quotationRecord.addFiles(req, {
+          dir: 'records/gallery',
+          profile: 'record'
+        });
+      const record = await QuotationRecord.findOne({
+        id: quotationRecord.id
+      })
+        .populate('User')
+        .populate('files');
+      res.created(record);
+    } catch (err) {
+      res.negotiate(err);
+    }
+  }
+};
