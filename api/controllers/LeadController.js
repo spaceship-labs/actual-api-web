4e61d66189bc191bdee8a7c8c40b2ad12a7276d1
Luis <luis19prz@gmail.com>
2018-03-22 10:02:58 -0500

    fix lead controller

--- api/controllers/LeadController.js
+++ api/controllers/LeadController.js
@@ -1,26 +1,29 @@
-var Promise = require('bluebird');
+var Promise = require("bluebird");
 
 module.exports = {
-
-	createLeadAndSendQuotation: function(req, res){
-		var form = req.allParams();
-		var quotationId = form.quotationId;
-		form.QuotationWeb = quotationId;
-		var createdLead;
-		Lead.create(form)
-			.then(function(_createdLead){
-				createdLead = _createdLead;
-				console.log('createdLead', createdLead);
-				var isCardProcessingError = false;
-				return Promise.resolve();
-				//return Email.sendQuotation(quotationId,req.activeStore, isCardProcessingError, createdLead);
-			})
-			.then(function(result){
-				res.json(createdLead);
-			})
-			.catch(function(err){
-				console.log('err create lead', err);
-				res.negotation(err);
-			});
-	}
+  createLeadAndSendQuotation: function(req, res) {
+    var form = req.allParams();
+    var quotationId = form.quotationId;
+    form.QuotationWeb = quotationId;
+    var createdLead;
+    Lead.create(form)
+      .then(function(_createdLead) {
+        createdLead = _createdLead;
+        console.log("createdLead", createdLead);
+        var isCardProcessingError = false;
+        return Email.sendQuotation(
+          quotationId,
+          req.activeStore,
+          isCardProcessingError,
+          createdLead
+        );
+      })
+      .then(function(result) {
+        res.json(createdLead);
+      })
+      .catch(function(err) {
+        console.log("err create lead", err);
+        res.negotation(err);
+      });
+  }
 };
