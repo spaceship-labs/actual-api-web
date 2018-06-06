184e1e0d1f1abeaf100c3ae513aa79acbdc43a15
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-06-06 16:05:23 -0500

    second banner field added

--- api/models/SiteBanner.js
+++ api/models/SiteBanner.js
@@ -1,14 +1,33 @@
 //APP COLLECTION
 module.exports = {
   attributes: {
-    filename: { type: 'string' },
-    name: { type: 'string' },
-    type: { type: 'string' },
-    typebase: { type: 'string' },
-    size: { type: 'integer' },
-    displayText: { type: 'string' },
-    displayUrl: { type: 'string' },
-    newTab: { type: 'boolean' },
+    filename: {
+      type: 'string'
+    },
+    name: {
+      type: 'string'
+    },
+    type: {
+      type: 'string'
+    },
+    typebase: {
+      type: 'string'
+    },
+    size: {
+      type: 'integer'
+    },
+    displayText: {
+      type: 'string'
+    },
+    displayUrl: {
+      type: 'string'
+    },
+    newTab: {
+      type: 'boolean'
+    },
+    secondBanner: {
+      type: 'boolean'
+    },
     Site: {
       model: 'site'
     }
