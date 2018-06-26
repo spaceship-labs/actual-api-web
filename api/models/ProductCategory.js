aac364bc4b77ed0541cf21ba6e5f220807460954
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-06-26 16:09:18 -0500

    complements and onKidsMenu added to ProductCategory model

--- api/models/ProductCategory.js
+++ api/models/ProductCategory.js
@@ -1,33 +1,39 @@
 //APP COLLECTION
 module.exports = {
-	migrate:'alter',
-	attributes:{
-		Name:{type:'string'},
-		Description:{type:'text'},
-		Keywords:{type:'string'},
-		Handle:{type:'string'},
-		IsMain:{type:'boolean'},
-		CategoryLevel:{type:'integer'},
-    productsNum: {type:'integer'},
-    Parents:{
-      collection: 'productcategory',
-      via:'Childs',
+  migrate: 'alter',
+  attributes: {
+    Name: { type: 'string' },
+    Description: { type: 'text' },
+    Keywords: { type: 'string' },
+    Handle: { type: 'string' },
+    IsMain: { type: 'boolean' },
+    CategoryLevel: { type: 'integer' },
+    productsNum: { type: 'integer' },
+    complement: {
+      type: 'boolean'
     },
-    Childs:{
-      collection: 'productcategory',
-      via:'Parents',
+    onKidsMenu: {
+      type: 'boolean'
     },
-    Filters:{
-      collection:'productfilter',
+    Parents: {
+      collection: 'productcategory',
+      via: 'Childs'
+    },
+    Childs: {
+      collection: 'productcategory',
+      via: 'Parents'
+    },
+    Filters: {
+      collection: 'productfilter',
       via: 'Categories'
     },
-    Products:{
+    Products: {
       collection: 'Product',
       via: 'Categories'
     },
-    Promotions:{
-      collection:'Promotion',
-      via:'Categories'
+    Promotions: {
+      collection: 'Promotion',
+      via: 'Categories'
     }
-	}
-}
+  }
+};
