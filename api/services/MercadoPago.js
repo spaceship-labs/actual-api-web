eaffeb5ae7b18c525d8df0549e3d6b4316dcf9b3
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-04-17 14:17:47 -0500

    minumum data send to mp api

--- api/services/MercadoPago.js
+++ api/services/MercadoPago.js
@@ -7,20 +7,14 @@ module.exports = {
   createOrder
 };
 
-function getTotalLinesAmount(lineItems, { amount }) {
-  let totalLines = lineItems.reduce((acum, lineItem) => {
-    acum += lineItem.unit_price * lineItem.quantity;
-    return acum;
-  }, 0);
-  totalLines = totalLines - amount;
-  return totalLines;
+function convertCentsToPesos(amount) {
+  return amount / 100;
 }
 
 async function createOrder(orderId, payment, req) {
   mercadopago.configurations.setAccessToken(accessToken);
   const userId = UserService.getCurrentUserId(req);
   const clientId = UserService.getCurrentUserClientId(req);
-
   const isValidStock = await StockService.validateQuotationStockById(orderId, req);
   if (!isValidStock) {
     throw new Error('Inventario no suficiente');
@@ -29,30 +23,15 @@ async function createOrder(orderId, payment, req) {
   if (!order) {
     throw new Error('Asigna una dirección de envio para continuar');
   }
-  const customerInfo = await getOrderCustomerInfo(payment, clientId);
-  const customerAddress = await getOrderCustomerAddress(order.Address);
-  const lineItems = await getOrderLineItems(order.id);
-  const payments = [payment];
-  const charges = getOrderCharges(order, payments);
-  const discountLine = getOrderDiscountLine(order, payments);
-  const paymentGroup = OrderService.getGroupByQuotationPayments(payments);
-  let totalLinesAmount = getTotalLinesAmount(lineItems, discountLine);
-  if (charges.length > 0) {
-    //Adding shipping fee
-    charges[0].amount = totalLinesAmount + convertToCents(order['deliveryFeePg' + paymentGroup]);
-  }
-  const cardCode = _.clone(customerInfo.CardCode);
-  delete customerInfo.CardCode;
-
+  const { E_Mail: email } = await Client.findOne({ id: clientId });
   const paymentParams = {
-    //Hace falta arreglar el objeto charge para que sea un monto y no un objeto
-    transaction_amount: charges[0].amount,
+    transaction_amount: order.total,
     token: payment.token,
-    description: 'Synergistic Granite Shirt',
+    description: 'Actual Description',
     installments: 1,
     payment_method_id: payment.payment_method_id,
     payer: {
-      email: 'salvador@hotmail.com'
+      email: payment.email || email
     }
   };
 
@@ -67,4 +46,10 @@ async function createOrder(orderId, payment, req) {
   mercadopagoOrder.responseData = JSON.stringify(mercadopagoOrder);
   mercadopagoOrder.QuotationWeb = orderId;
   mercadopagoOrder.UserWeb = userId;
+
+  //Validar si es pago con SPEI
+
+  mercadopagoOrder.amount = convertCentsToPesos(mercadopagoOrder.amount);
+  delete mercadopagoOrder.id;
+  return await MercadoPagoOrder.create(mercadopagoOrder);
 }
