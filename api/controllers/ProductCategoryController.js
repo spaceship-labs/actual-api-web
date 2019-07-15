df2160df8c13cbf2da0935f328ec037ffe3f9d90
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-07-15 11:45:30 -0500

    featured products by category logic added, fields in product and productcategory added

--- api/controllers/ProductCategoryController.js
+++ api/controllers/ProductCategoryController.js
@@ -2,121 +2,127 @@ var _ = require('underscore');
 var Promise = require('bluebird');
 
 module.exports = {
-  find: function(req, res){
+  find: function(req, res) {
     var form = req.params.all();
     var model = 'productcategory';
     var extraParams = {
-      searchFields: ['Name','Handle']
+      searchFields: ['Name', 'Handle']
     };
-    Common.find(model, form, extraParams).then(function(result){
-      res.ok(result);
-    })
-    .catch(function(err){
-      console.log(err);
-      res.negotiate(err);
-    });
+    Common.find(model, form, extraParams)
+      .then(function(result) {
+        res.ok(result);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.negotiate(err);
+      });
   },
 
-
-  getAllCategories: function(req, res){
+  getAllCategories: function(req, res) {
     ProductCategory.find()
       .sort('CategoryLevel ASC')
       .populate('Childs')
       .populate('Parents')
-      .then(function(result){
+      .populate('FeaturedProducts')
+      .then(function(result) {
         res.ok(result);
       })
-      .catch(function(err){
+      .catch(function(err) {
         res.negotiate(err);
       });
   },
 
-
-  getCategoriesTree: function(req, res){
+  getCategoriesTree: function(req, res) {
     Promise.join(
-      ProductCategory.find({CategoryLevel:1}).populate('Childs'),
-      ProductCategory.find({CategoryLevel:2}).populate('Childs'),
-      ProductCategory.find({CategoryLevel:3}).populate('Parents')
-    ).then(function(groups){
-      var categoriesLv1 = groups[0] || [];
-      var categoriesLv2 = groups[1] || [];
-      var categoriesLv3 = groups[2] || [];
-      var categoryTree = CategoryService.buildCategoriesTree(categoriesLv1, categoriesLv2, categoriesLv3);
-      res.json(categoryTree);
-    })
-    .catch(function(err){
-      console.log(err);
-      res.negotiate(err);
-    });
+      ProductCategory.find({ CategoryLevel: 1 }).populate('Childs'),
+      ProductCategory.find({ CategoryLevel: 2 }).populate('Childs'),
+      ProductCategory.find({ CategoryLevel: 3 }).populate('Parents')
+    )
+      .then(function(groups) {
+        var categoriesLv1 = groups[0] || [];
+        var categoriesLv2 = groups[1] || [];
+        var categoriesLv3 = groups[2] || [];
+        var categoryTree = CategoryService.buildCategoriesTree(
+          categoriesLv1,
+          categoriesLv2,
+          categoriesLv3
+        );
+        res.json(categoryTree);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.negotiate(err);
+      });
   },
 
-  getCategoriesGroups: function(req, res){
+  getCategoriesGroups: function(req, res) {
     Promise.join(
-      ProductCategory.find({CategoryLevel:1}).populate('Childs'),
-      ProductCategory.find({CategoryLevel:2}).populate('Childs'),
-      ProductCategory.find({CategoryLevel:3}).populate('Parents')
-    ).then(function(categoriesGroups){
-      res.json(categoriesGroups);
-    })
-    .catch(function(err){
-      console.log(err);
-      res.negotiate(err);
-    });
-
+      ProductCategory.find({ CategoryLevel: 1 }).populate('Childs'),
+      ProductCategory.find({ CategoryLevel: 2 }).populate('Childs'),
+      ProductCategory.find({ CategoryLevel: 3 }).populate('Parents')
+    )
+      .then(function(categoriesGroups) {
+        res.json(categoriesGroups);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.negotiate(err);
+      });
   },
 
-  getMainCategories: function(req, res){
-    ProductCategory.find({IsMain:true}).then(function(categories){
-      res.json(categories);
-    })
-    .catch(function(err){
-      console.log(err);
-      res.negotiate(err);
-    });
+  getMainCategories: function(req, res) {
+    ProductCategory.find({ IsMain: true })
+      .then(function(categories) {
+        res.json(categories);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.negotiate(err);
+      });
   },
 
-  findById: function(req, res){
+  findById: function(req, res) {
     var form = req.params.all();
     var id = form.id;
-    ProductCategory.findOne({id:id}).populate('Childs').populate('Parents')
-      .then(function(category){
+    ProductCategory.findOne({ id: id })
+      .populate('Childs')
+      .populate('Parents')
+      .populate('FeaturedProducts')
+      .then(function(category) {
         res.json(category);
       })
-      .catch(function(err){
-        console.log(err);
-        res.negotiate(err);
-      });
-
-  },
-
-  getCategory: function(req, res){
-    var form = req.params.all();
-    var handle = form.handle;
-    ProductCategory.findOne({Handle:handle}).populate('Products')
-      .then(function(category){
-        res.json(category);
-      })
-      .catch(function(err){
+      .catch(function(err) {
         console.log(err);
         res.negotiate(err);
       });
   },
 
-  findByHandle: function(req, res){
+  getCategory: function(req, res) {
     var form = req.params.all();
     var handle = form.handle;
-    ProductCategory.findOne({Handle:handle})
+    ProductCategory.findOne({ Handle: handle })
+      .populate('Products')
+      .then(function(category) {
+        res.json(category);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.negotiate(err);
+      });
+  },
+
+  findByHandle: function(req, res) {
+    var form = req.params.all();
+    var handle = form.handle;
+    ProductCategory.findOne({ Handle: handle })
       .populate('Childs')
       .populate('Filters')
-      .then(function(category){
+      .then(function(category) {
         res.json(category);
       })
-      .catch(function(err){
+      .catch(function(err) {
         console.log(err);
         res.negotiate(err);
-      });    
+      });
   }
-
 };
-
-
--- api/models/Product.js
+++ api/models/Product.js
@@ -1,196 +1,196 @@
 //APP/SAP COLLECTION
 module.exports = {
-    schema: true,
-    migrate: 'alter',
-    tableName: 'Product',
-    attributes: {
-      /*----------------/
+  schema: true,
+  migrate: 'alter',
+  tableName: 'Product',
+  attributes: {
+    /*----------------/
           #SAP FIELDS
       /*---------------*/
-      ItemCode:{
-        type:'string',
-        //primaryKey:true
-      },
-      ItemName:{type:'string'},
-      ItmsGrpCod:{ //Brand
-          type:'integer',
-          model:'productbrand'
-      },
-      ItmsGrpNam:{type:'string'},
-      SuppCatNum:{type:'string',size:17},
-      CodeBars:{type:'string'},
-      OnHand:{type:'float'},
-      IsCommited:{type:'float'},
-      OnOrder:{type:'float'},
-      Available:{type:'float'},
-      PicturName:{type:'string'},
-      SalUnitMsr:{type:'string'},
-      U_LINEA:{type:'string',size:60},
-      U_PRODUCTO:{type:'string',size:60},
-      U_COLOR:{type:'string',size:60},
-      U_garantia:{type:'string',size:60},
-      U_disenado_en:{type:'string',size:60},
-      U_ensamblado_en:{type:'string',size:60},
-      U_importador: {type:'string',size:60},
-      U_pctPuntos:{type:'float'},
-      U_FAMILIA:{type:'string', size:30},
-      U_Empresa:{type:'string'},
-      nameSA: {type:'string',columnName:'EmpresaName'},
-      U_ClaveProdServ: {type:'string'},
-      U_ClaveUnidad: {type:'string'},
+    ItemCode: {
+      type: 'string'
+      //primaryKey:true
+    },
+    ItemName: { type: 'string' },
+    ItmsGrpCod: {
+      //Brand
+      type: 'integer',
+      model: 'productbrand'
+    },
+    ItmsGrpNam: { type: 'string' },
+    SuppCatNum: { type: 'string', size: 17 },
+    CodeBars: { type: 'string' },
+    OnHand: { type: 'float' },
+    IsCommited: { type: 'float' },
+    OnOrder: { type: 'float' },
+    Available: { type: 'float' },
+    PicturName: { type: 'string' },
+    SalUnitMsr: { type: 'string' },
+    U_LINEA: { type: 'string', size: 60 },
+    U_PRODUCTO: { type: 'string', size: 60 },
+    U_COLOR: { type: 'string', size: 60 },
+    U_garantia: { type: 'string', size: 60 },
+    U_disenado_en: { type: 'string', size: 60 },
+    U_ensamblado_en: { type: 'string', size: 60 },
+    U_importador: { type: 'string', size: 60 },
+    U_pctPuntos: { type: 'float' },
+    U_FAMILIA: { type: 'string', size: 30 },
+    U_Empresa: { type: 'string' },
+    nameSA: { type: 'string', columnName: 'EmpresaName' },
+    U_ClaveProdServ: { type: 'string' },
+    U_ClaveUnidad: { type: 'string' },
 
+    //FIELDS FROM PRICE TABLE
+    PriceList: { type: 'integer', size: 4 },
+    Price: { type: 'float' },
+    Currency: { type: 'string', size: 3 },
+    Discount: { type: 'float' },
+    DiscountPrice: { type: 'float' },
 
-      //FIELDS FROM PRICE TABLE
-      PriceList:{type:'integer', size: 4},
-      Price:{type:'float'},
-      Currency:{type:'string',size:3},
-      Discount:{type:'float'},
-      DiscountPrice:{type:'float'},
-
-      /*----------------/
+    /*----------------/
           #EXTRA FIELDS
       /*---------------*/
-      Name:{type:'string'},
-      Handle:{type:'string'},
-      Description:{type:'text'},
-      deliveryType:{type:'string'},
-      //CHECAR Model:{type:'string'},
+    Name: { type: 'string' },
+    Handle: { type: 'string' },
+    Description: { type: 'text' },
+    deliveryType: { type: 'string' },
+    //CHECAR Model:{type:'string'},
 
-      Brand:{ //BrandExtra
-          type:'integer',
-          model:'productbrand'
-      },
-
-      Grouper:{type:'string',size:17},
-      SA:{type:'text'},
-      MainFeatures:{type:'text'},
-      Restrictions:{type:'text'},
-      Color: {type:'string'},
-      DetailedColor:{type:'string'},
-      GuaranteeText:{type:'text'},
-      GuaranteeUnit:{type:'integer'},
-      GuaranteeUnitMsr: {type:'string',size:30},
-      Seats:{type:'string'},
-      DesignedInCountry:{type:'string'},
-      MadeInCountry:{type:'string'},
-      EnsembleTime:{type:'string'},
-      Ensemble:{type:'string'},
-      PackageContent:{type:'text'},
-      CommercialPieces:{type:'integer'},
-      DeliveryPieces:{type:'integer'},
-      Length:{type:'float'},
-      LengthUnitMsr:{type:'string'},
-      Width:{type:'float'},
-      WidthUnitMsr:{type:'string'},
-      Height:{type:'float'},
-      HeightUnitMsr:{type:'string'},
-      Volume:{type:'float'},
-      VolumeUnitMsr:{type:'string'},
-      Weight:{type:'float'},
-      WeightUnitMsr:{type:'string'},
-      icon_filename:{type:'string'},
-      icon_name:{type:'string'},
-      icon_type:{type:'string'},
-      icon_typebase:{type:'string'},
-      icon_size:{type:'integer'},
-      icon_description:{type:'string'},
-      Video:{type:'text'},
-      Characteristics:{type:'text'},
-      OnOffline:{type:'boolean'},
-      OnStudio:{type:'boolean'},
-      OnHome:{type:'boolean'},
-      OnKids:{type:'boolean'},
-      OnAmueble:{type:'boolean'},
-      ImagesOrder:{type:'string'},
-      Conservation: {type:'text'},
-      Service:{type:'boolean'},
-
-      CheckedPhotos : {type:'boolean'},
-      CheckedStructure : {type:'boolean'},
-      CheckedDescription : {type:'boolean'},
-      CheckedPackage : {type:'boolean'},
-      CheckedFeatures: {type: 'boolean'},
-
-      ChairBackHeight: {type:'float'},
-      ChairBackHeightUnitMsr: {type:'string'},
-      SeatHeight: {type:'float'},
-      SeatHeightUnitMsr: {type:'string'},
-      ArmRestHeight:{type:'float'},
-      ArmRestHeightUnitMsr:{type:'string'},
-      Depth:{type:'float'},
-      DepthUnitMsr:{type:'string'},
-
-      freeSale: {type:'boolean'},
-      freeSaleStock: {
-        type:'integer',
-        defaultsTo: 0
-      },
-      freeSaleDeliveryDays: {
-        type:'integer',
-        defaultsTo: 0
-      },
-      deliveryType:{
-        type:'string',
-        enum:[
-          'bigticket',
-          'softline'
-        ]
-      },      
-      slowMovement:{type:'boolean'},
-      seenTimes: {type:'integer'},
-      immediateDelivery:{type:'boolean'},
-
-      actual_studio_merida:{type:'integer'},
-      actual_studio_malecon:{type:'integer'},
-      actual_studio_playa_del_carmen:{type:'integer'},
-      actual_studio_cumbres:{type:'integer'},
-      actual_home_xcaret:{type:'integer'},
-      actual_home_merida:{type:'integer'},
-      proyectos:{type:'integer'},
-      actual_proyect:{type:'integer'},
-      actual_puerto_cancun:{type:'integer'},
-      excludeWeb:{type:'boolean'},
-
-      //RELATIONS
-      files: {
-        collection: 'productfile',
-        via:'Product',
-        //excludeSync: true
-      },
-      Sizes: {
-        collection: 'productsize',
-        via:'Product',
-      },
-      Categories:{
-        collection: 'ProductCategory',
-        via: 'Products',
-        dominant: true
-      },
-      FilterValues:{
-        collection:'ProductFilterValue',
-        via: 'Products',
-        dominant: true
-      },
-      Groups: {
-        collection: 'ProductGroup',
-        via: 'Products'
-      },
-      CustomBrand: {
-        model: 'CustomBrand'
-      },
-      QuotationDetails:{
-        collection:'QuotationDetailWeb',
-        via:'Product'
-      },
-      Promotions:{
-        collection: 'Promotion',
-        via:'Products'
-      },
-      PackageRules:{
-        collection:'PackageRule',
-        via:'Product'
-      }
-
+    Brand: {
+      //BrandExtra
+      type: 'integer',
+      model: 'productbrand'
     },
 
-}
+    Grouper: { type: 'string', size: 17 },
+    SA: { type: 'text' },
+    MainFeatures: { type: 'text' },
+    Restrictions: { type: 'text' },
+    Color: { type: 'string' },
+    DetailedColor: { type: 'string' },
+    GuaranteeText: { type: 'text' },
+    GuaranteeUnit: { type: 'integer' },
+    GuaranteeUnitMsr: { type: 'string', size: 30 },
+    Seats: { type: 'string' },
+    DesignedInCountry: { type: 'string' },
+    MadeInCountry: { type: 'string' },
+    EnsembleTime: { type: 'string' },
+    Ensemble: { type: 'string' },
+    PackageContent: { type: 'text' },
+    CommercialPieces: { type: 'integer' },
+    DeliveryPieces: { type: 'integer' },
+    Length: { type: 'float' },
+    LengthUnitMsr: { type: 'string' },
+    Width: { type: 'float' },
+    WidthUnitMsr: { type: 'string' },
+    Height: { type: 'float' },
+    HeightUnitMsr: { type: 'string' },
+    Volume: { type: 'float' },
+    VolumeUnitMsr: { type: 'string' },
+    Weight: { type: 'float' },
+    WeightUnitMsr: { type: 'string' },
+    icon_filename: { type: 'string' },
+    icon_name: { type: 'string' },
+    icon_type: { type: 'string' },
+    icon_typebase: { type: 'string' },
+    icon_size: { type: 'integer' },
+    icon_description: { type: 'string' },
+    Video: { type: 'text' },
+    Characteristics: { type: 'text' },
+    OnOffline: { type: 'boolean' },
+    OnStudio: { type: 'boolean' },
+    OnHome: { type: 'boolean' },
+    OnKids: { type: 'boolean' },
+    OnAmueble: { type: 'boolean' },
+    ImagesOrder: { type: 'string' },
+    Conservation: { type: 'text' },
+    Service: { type: 'boolean' },
+
+    CheckedPhotos: { type: 'boolean' },
+    CheckedStructure: { type: 'boolean' },
+    CheckedDescription: { type: 'boolean' },
+    CheckedPackage: { type: 'boolean' },
+    CheckedFeatures: { type: 'boolean' },
+
+    ChairBackHeight: { type: 'float' },
+    ChairBackHeightUnitMsr: { type: 'string' },
+    SeatHeight: { type: 'float' },
+    SeatHeightUnitMsr: { type: 'string' },
+    ArmRestHeight: { type: 'float' },
+    ArmRestHeightUnitMsr: { type: 'string' },
+    Depth: { type: 'float' },
+    DepthUnitMsr: { type: 'string' },
+
+    freeSale: { type: 'boolean' },
+    freeSaleStock: {
+      type: 'integer',
+      defaultsTo: 0
+    },
+    freeSaleDeliveryDays: {
+      type: 'integer',
+      defaultsTo: 0
+    },
+    deliveryType: {
+      type: 'string',
+      enum: ['bigticket', 'softline']
+    },
+    slowMovement: { type: 'boolean' },
+    seenTimes: { type: 'integer' },
+    immediateDelivery: { type: 'boolean' },
+
+    actual_studio_merida: { type: 'integer' },
+    actual_studio_malecon: { type: 'integer' },
+    actual_studio_playa_del_carmen: { type: 'integer' },
+    actual_studio_cumbres: { type: 'integer' },
+    actual_home_xcaret: { type: 'integer' },
+    actual_home_merida: { type: 'integer' },
+    proyectos: { type: 'integer' },
+    actual_proyect: { type: 'integer' },
+    actual_puerto_cancun: { type: 'integer' },
+    excludeWeb: { type: 'boolean' },
+
+    //RELATIONS
+    files: {
+      collection: 'productfile',
+      via: 'Product'
+      //excludeSync: true
+    },
+    Sizes: {
+      collection: 'productsize',
+      via: 'Product'
+    },
+    Categories: {
+      collection: 'ProductCategory',
+      via: 'Products',
+      dominant: true
+    },
+    FeaturedCategories: {
+      collection: 'ProductCategory',
+      via: 'FeaturedProducts'
+    },
+    FilterValues: {
+      collection: 'ProductFilterValue',
+      via: 'Products',
+      dominant: true
+    },
+    Groups: {
+      collection: 'ProductGroup',
+      via: 'Products'
+    },
+    CustomBrand: {
+      model: 'CustomBrand'
+    },
+    QuotationDetails: {
+      collection: 'QuotationDetailWeb',
+      via: 'Product'
+    },
+    Promotions: {
+      collection: 'Promotion',
+      via: 'Products'
+    },
+    PackageRules: {
+      collection: 'PackageRule',
+      via: 'Product'
+    }
+  }
+};
