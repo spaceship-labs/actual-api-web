3fc9ec7a957435caf49ad5777c993fb8e27c3202
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-08-22 16:38:56 -0500

    wip

--- api/controllers/StoreController.js
+++ api/controllers/StoreController.js
@@ -1,63 +1,66 @@
 module.exports = {
   find: function(req, res) {
     Store.find()
-      .then(function(stores){
+      .then(function(stores) {
         return res.json(stores);
       })
-      .catch(function(err){
-        console.log(err);
-        res.negotiate(err);
-      });      
-  },
-
-  getPromosByStore: function(req, res){
-    var form = req.params.all();
-    var id = form.id;
-    var currentDate = new Date();
-    var queryPromo = {
-      startDate: {'<=': currentDate},
-      endDate: {'>=': currentDate},
-    };
-
-    if(!id || id === 'null'){
-      id = SiteService.getDefaultActiveStoreId(req);
-    }
-
-    Store.findOne({id:id}).populate('Promotions', queryPromo)
-      .then(function(company){
-        res.json(company.Promotions);
-      })
-      .catch(function(err){
+      .catch(function(err) {
         console.log(err);
         res.negotiate(err);
       });
   },
 
-  getPackagesByCurrentStore: function(req, res){
-    var form        = req.params.all();
-    var id          = form.id;
+  getPromosByStore: function(req, res) {
+    var form = req.params.all();
+    var id = form.id;
+    var currentDate = new Date();
+    var queryPromo = {
+      startDate: { '<=': currentDate },
+      endDate: { '>=': currentDate }
+    };
+
+    if (!id || id === 'null') {
+      id = SiteService.getDefaultActiveStoreId(req);
+    }
+
+    Store.findOne({ id: id })
+      .populate('Promotions', queryPromo)
+      .then(function(company) {
+        res.json(company.Promotions);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.negotiate(err);
+      });
+  },
+
+  getPackagesByCurrentStore: function(req, res) {
+    var form = req.params.all();
+    var id = form.id;
     var activeStoreId = req.activeStore.id;
     var queryPromos = Search.getPromotionsQuery();
-    
-    Store.findOne({id:activeStoreId})
+
+    Store.findOne({ id: activeStoreId })
       .populate('PromotionPackages', queryPromos)
-      .then(function(store){
+      .then(function(store) {
+        console.log('store: ', store);
+
         res.json(store.PromotionPackages);
       })
-      .catch(function(err){
+      .catch(function(err) {
         console.log(err);
         res.negotiate(err);
       });
   },
 
   getAll: function(req, res) {
-    Store.find().then(function(stores) {
-      return res.json(stores);
-    })
-    .catch(function(err){
-      console.log(err);
-      res.negotiate(err);
-    });    
-  },
-  
+    Store.find()
+      .then(function(stores) {
+        return res.json(stores);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.negotiate(err);
+      });
+  }
 };
