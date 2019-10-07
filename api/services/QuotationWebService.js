c4150c59503199b9a1144cdd5b241132688cad33
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-10-07 10:32:59 -0500

    removed client grom unregisteredclient

--- api/services/QuotationWebService.js
+++ api/services/QuotationWebService.js
@@ -1,12 +1,7 @@
 const assignClient = async (name, email, phone, id) => {
-  // Validar is ya existe un cliente registrado
-  const client = await Client.findOne({ E_Mail: email });
   const unregisteredClient = await UnregisteredClient.findOne({ email });
-  if (client || unregisteredClient) {
-    return await QuotationWeb.update(
-      { id },
-      client ? { Client: client.id } : { UnregisteredClient: unregisteredClient.id }
-    );
+  if (unregisteredClient) {
+    return await QuotationWeb.update({ id }, { UnregisteredClient: unregisteredClient.id });
   } else {
     const { id: newUnregisteredClientId } = await UnregisteredClient.create({ name, email, phone });
     return await QuotationWeb.update({ id }, { UnregisteredClient: newUnregisteredClientId });
